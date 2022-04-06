b69c33139137fe0f0eea474d382827b69f1c9712
This is a test <testuser@test.ru>
2022-04-06 09:39:44 -0300

    Test PSSRV-37508

---
+++ PSSRV-37508/test.py
@@ -0,0 +1,18 @@
+#!/auto/grqr-prod/bin/cigprod64/tp/miniconda3/envs/grqr-11.04/bin/python
+
+import math
+# Assign values to x and n
+x = 4
+n = 3
+
+# Method 1
+power = x ** n
+print("%d to the power %d is %d" % (x,n,power))
+
+# Method 2
+power = pow(x,n)
+print("%d to the power %d is %d" % (x,n,power))
+
+# Method 3
+power = math.pow(2,6.5)
+print("%d to the power %d is %5.2f" % (x,n,power))
